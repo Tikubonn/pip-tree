46924d5e8b37c567c08f6f1f63d86d2422b1d901
tikubonn <7812951+Tikubonn@users.noreply.github.com>
2026-02-28 00:36:43 +0900

    update: 不要だと判断したため --dump-root-only オプションを削除しました。 update: --json オプションの出力内容が変更されました。具体的に説明すると、出力データの内容が pip show コマンドの内容を解析したものでなく importlib.metadata.metadata 関数の実行結果を参照するようになりました。

--- src/pip_tree.py
+++ src/pip_tree.py
@@ -1,136 +1,129 @@
 
-import re 
+import os
+import re
 import sys 
-import json 
+import json
+import argparse
+import importlib.metadata
 import opensafer
-import subprocess 
-from argparse import ArgumentParser
+import subprocess
 
-_REGEXP_PIP_FREEZE_EQUAL:re.Pattern = re.compile(r"^(\S+?)==\S+$")
-_REGEXP_PIP_FREEZE_AT:re.Pattern = re.compile(r"^(\S+?) @ .*$")
-_REGEXP_PIP_FREEZE_EDITABLE:re.Pattern = re.compile(r"^# Editable install with no version control \((\S+?)==.*$")
-_REGEXP_PIP_SHOW_ENTRY:re.Pattern = re.compile(r"(.*?):(.*)")
+class _PIPTree:
 
-def _list_installed_package () -> list[str]:
-  result = []
-  process = subprocess.run(
-    ["pip", "freeze"], 
-    shell=True, 
-    text=True, 
-    stdin=subprocess.DEVNULL, 
-    stdout=subprocess.PIPE, 
-    stderr=subprocess.PIPE, 
-    check=True
-  )
-  for line in process.stdout.strip().split("\n"):
-    match_result = _REGEXP_PIP_FREEZE_EQUAL.match(line)
-    if match_result:
-      name, = match_result.groups()
-      result.append(name)
-      continue
-    match_result = _REGEXP_PIP_FREEZE_AT.match(line)
-    if match_result:
-      name, = match_result.groups()
-      result.append(name)
-      continue
-    match_result = _REGEXP_PIP_FREEZE_EDITABLE.match(line)
-    if match_result:
-      name, = match_result.groups()
-      result.append(name)
-      continue
-  return result
+  _REGEXP_PACKAGE_EXPRESSIONS:"typing.ClassVar[re.Pattern]" = [
+    re.compile(r"^(\S+)==\S+$"),
+    re.compile(r"^(\S+) @ \S+$"),
+    re.compile(r"^# Editable install with no version control \((\S+)==\S+\)$"),
+    re.compile(r"^(\S+)$"),
+  ]
+  
+  _REGEXP_PIP_SHOW_NAME:"typing.ClassVar[re.Pattern]" = re.compile(r"^Name: (\S+)$")
+  _REGEXP_PIP_SHOW_REQUIRES:"typing.ClassVar[re.Pattern]" = re.compile(r"^Requires: (.*)$")
 
-def _get_package_info (package_name:str) -> "dict[str, typing.Any]":
-  result = {}
-  process = subprocess.run(
-    ["pip", "show", package_name], 
-    shell=True, 
-    text=True, 
-    stdin=subprocess.DEVNULL, 
-    stdout=subprocess.PIPE, 
-    stderr=subprocess.PIPE, 
-    check=True
-  )
-  for line in process.stdout.strip().split("\n"):
-    match_result = _REGEXP_PIP_SHOW_ENTRY.match(line)
-    if match_result:
-      key, value = map(str.strip, match_result.groups())
-      if value:
-        if key == "Requires":
-          result[key] = [p.strip() for p in value.split(",")]
-        elif key == "Required-by":
-          result[key] = [p.strip() for p in value.split(",")]
-        else:
-          result[key] = value
-  return result
+  def __init__ (self):
+    self.package_dependency_table = {}
+    self.package_table = {}
 
-def _get_package_info_tree (package_names:list[str]) -> "list[dict[str, typing.Any]]":
-  package_infos = list()
-  package_info_table = dict()
-  for package_name in package_names:
-    if package_name not in package_info_table:
-      package_info_table[package_name] = _get_package_info(package_name)
-    package_infos.append(package_info_table[package_name])
-  tmp = package_infos[:]
-  while tmp:
-    package_info = tmp.pop()
-    if "_Dependencies" not in package_info:
-      for pkg in package_info.get("Requires", []):
-        if pkg not in package_info_table:
-          package_info_table[pkg] = _get_package_info(pkg)
-        pkg_info = package_info_table[pkg]
-        package_info.setdefault("_Dependencies", [])
-        package_info["_Dependencies"].append(pkg_info)
-        tmp.append(pkg_info)
-  return package_infos
+  def list_installed_package () -> list[str]:
+    process = subprocess.run(
+      ["pip", "freeze"],
+      stdin=subprocess.DEVNULL,
+      stdout=subprocess.PIPE,
+      stderr=subprocess.PIPE,
+      text=True,
+      check=True
+    )
+    installed_packages = []
+    for line in process.stdout.split("\n"):
+      for regexp in self._REGEXP_PACKAGE_EXPRESSIONS:
+        match = regexp.match(line)
+        if match:
+          installed_packages.append(match.group(1))
+          break
+    return installed_packages
 
-def _dump_package_info_tree (
-  package_infos:"list[dict[str, typing.Any]]", 
+  def get_package_dependencies (self, package:str) -> list[str]:
+    if package not in self.package_dependency_table:
+      process = subprocess.run(
+        ["pip", "show", package],
+        stdin=subprocess.DEVNULL,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        check=True,
+        text=True
+      )
+      name = ""
+      requires = []
+      for line in process.stdout.split("\n"):
+        match = self._REGEXP_PIP_SHOW_NAME.match(line)
+        if match:
+          name, = match.groups()
+          continue
+        match = self._REGEXP_PIP_SHOW_REQUIRES.match(line)
+        if match:
+          requires_src, = match.groups()
+          if requires_src:
+            requires = requires_src.split(", ")
+          continue
+      self.package_dependency_table[package] = requires
+    return self.package_dependency_table[package]
+
+  def get_package (self, package:str) -> "dict[str, typing.Any]":
+    if package not in self.package_table:
+      metadata = importlib.metadata.metadata(package)
+      metadata_dict = metadata.json
+      metadata_dict["_dependencies"] = []
+      for dependency in self.get_package_dependencies(package):
+        pkg = self.get_package(dependency)
+        metadata_dict["_dependencies"].append(pkg)
+      self.package_table[package] = metadata_dict
+    return self.package_table[package]
+
+def dump_package_tree (
+  packages:"list[dict[str, typing.Any]]", 
   *, 
   file:"io.TextIOBase", 
   indent:int, 
   _total_indent:int=0):
-  for packageinfo in package_infos:
+  for package in packages:
     if 0 < _total_indent:
-      indentation = "{:s}- ".format(" " * _total_indent)
+      indentation = " " * _total_indent + "- "
     else:
       indentation = ""
     print("{:s}{:s}=={:s}".format(
       indentation, 
-      packageinfo["Name"],
-      packageinfo["Version"]
+      package["name"], 
+      package["version"]
     ), file=file)
-    _dump_package_info_tree(
-      packageinfo.get("_Dependencies", []), 
+    dump_package_tree(
+      package.get("_dependencies", []), 
+      file=file, 
       indent=indent, 
-      file=file,
       _total_indent=_total_indent + indent
     )
 
 def main ():
-  parser = ArgumentParser(description="Dump installed package info by pip.")
+  parser = argparse.ArgumentParser(description="Dump installed package info by pip.")
   parser.add_argument("packages", nargs="*", help="Package names for dump.")
   parser.add_argument("--json", action="store_true", help="Dump information as JSON format.")
   parser.add_argument("--indent", nargs="?", default=2, help="Amount of indentation depth.")
-  parser.add_argument("--dump-root-only", action="store_true", help="If enabled, show root packages only.")
   parser.add_argument("-o", "--output-file", type=str, default="", help="Path of output file. (default is stdout).")
   args = parser.parse_args()
+  pip_tree = _PIPTree()
   if args.packages:
     packages = args.packages
   else:
-    packages = _list_installed_package()
-  package_infos = _get_package_info_tree(packages)
-  if args.dump_root_only: #--dump-root-onlyが真ならば親をもたないパッケージのみ抽出する。
-    package_infos = [package_info for package_info in package_infos if not package_info.get("Required-by", [])]
+    packages = pip_tree.list_installed_package()
   if args.output_file:
     stream = opensafer.open_safer(args.output_file, "w", encoding="utf-8")
   else:
-    stream = sys.stdout
+    stream = os.fdopen(os.dup(sys.stdout.fileno()), "w", encoding="utf-8")
   with stream:
+    package_tree = [pip_tree.get_package(p) for p in packages]
     if args.json:
-      json.dump(package_infos, stream, indent=args.indent)
+      json.dump(package_tree, stream, indent=args.indent)
     else:
-      _dump_package_info_tree(package_infos, file=stream, indent=args.indent)
+      dump_package_tree(package_tree, file=stream, indent=args.indent)
 
 if __name__ == "__main__":
   main()
